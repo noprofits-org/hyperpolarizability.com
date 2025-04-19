943572daeb2619b21fe097af5a601c246c2e673f
Peter Johnston <petervjohnston@gmail.com>
2025-04-18 19:20:56 -0700

    Initial commit of hyperpolarizability.com site

---
+++ site.hs
@@ -0,0 +1,89 @@
+{-# LANGUAGE OverloadedStrings #-}
+import           Data.Monoid (mappend)
+import           Hakyll
+import           Text.Pandoc.Options
+import           Text.Pandoc.Extensions (pandocExtensions)
+import qualified Data.Set as S
+
+-- | Citation-enabled compiler
+bibtexCompiler :: String -> String -> Compiler (Item String)
+bibtexCompiler cslFileName bibFileName = do
+    csl <- load $ fromFilePath cslFileName
+    bib <- load $ fromFilePath bibFileName
+
+    let readerOptions = defaultHakyllReaderOptions
+        writerOptions = defaultHakyllWriterOptions
+            { writerHTMLMathMethod = MathJax ""
+            }
+
+    getResourceBody
+        >>= readPandocBiblio readerOptions csl bib
+        >>= return . writePandocWith writerOptions
+
+main :: IO ()
+main = hakyll $ do
+    match "images/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "js/*" $ do
+        route idRoute
+        compile copyFileCompiler
+
+    match "bib/style.csl" $ do
+        compile cslCompiler
+
+    match "bib/bibliography.bib" $ do
+        compile biblioCompiler
+
+    match "css/*" $ do
+        route   idRoute
+        compile compressCssCompiler
+
+    match (fromList ["about.rst", "contact.markdown"]) $ do
+        route   $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+            >>= relativizeUrls
+
+    match "posts/*" $ do
+        route $ setExtension "html"
+        compile $ bibtexCompiler "bib/style.csl" "bib/bibliography.bib"
+            >>= loadAndApplyTemplate "templates/post.html" postCtx
+            >>= loadAndApplyTemplate "templates/default.html" postCtx
+            >>= relativizeUrls
+
+    create ["archive.html"] $ do
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll "posts/*"
+            let archiveCtx =
+                    listField "posts" postCtx (return posts) `mappend`
+                    constField "title" "Archives"            `mappend`
+                    defaultContext
+
+            makeItem ""
+                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+                >>= relativizeUrls
+
+    match "index.html" $ do
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll "posts/*"
+            let indexCtx =
+                    listField "posts" postCtx (return posts) `mappend`
+                    defaultContext
+
+            getResourceBody
+                >>= applyAsTemplate indexCtx
+                >>= loadAndApplyTemplate "templates/default.html" indexCtx
+                >>= relativizeUrls
+
+    match "templates/*" $ compile templateCompiler
+
+postCtx :: Context String
+postCtx =
+    constField "isPost" "true" <>
+    dateField "date" "%B %e, %Y" <>
+    defaultContext
